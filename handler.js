56478be3df8de6fcc7fdb17472d25d566c172f11
Marcus Weiner <marcus.weiner@gmail.com>
2018-05-18 18:47:32 +0200

    :sparkles: Store sitemap per month

--- handler.js
+++ handler.js
@@ -4,6 +4,7 @@ const request = require('request-promise-native');
 const imageSize = require('probe-image-size');
 const aws = require('aws-sdk');
 const slugify = require('slugify');
+const { js2xml, xml2js } = require('xml-js');
 
 const LambdaResponseMixin = (Base) =>
     class extends Base {
@@ -53,6 +54,8 @@ module.exports.updateReport = async function(event) {
         url,
     });
     await storeArticle(out, urlBase, url, payload.id);
+
+    await storeMonthSitemap(urlBase, url);
 
     return {
         body: JSON.stringify({success: true}),
@@ -109,4 +112,67 @@ function storeArticle(content, urlBase, url, id) {
             WebsiteRedirectLocation: `/${url}`,
         }).promise();
     }
+}
+
+function storeMonthSitemap(urlBase, url) {
+    let sitemapExisting;
+    try {
+        sitemapExisting = (await s3.getObject({Key: `${urlBase}/sitemap.xml`}).promise()).Body;
+    } catch(e) {
+        if (e.code !== 'NoSuchKey') {
+            throw e;
+        }
+    }
+    let sitemapMonth = !sitemapExisting
+        ? {
+            _declaration: {
+                _attributes: {
+                    version: '1.0',
+                    encoding: 'utf-8',
+                },
+            },
+            urlset: {
+                _attributes: {
+                    xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+                },
+                urls: [],
+            },
+        }
+        : xml2js(sitemapExisting, { compact: true });
+    if (!Array.isArray(sitemapMonth.urlset.urls)) {
+        sitemapMonth.urlset.urls = [sitemapMonth.urlset.urls];
+    }
+
+    const urlOrigin = `http://${process.env.BUCKET_NAME}.s3-website.eu-central-1.amazonaws.com/`;
+    const sitemapEntry = {
+        loc: { '_text': urlOrigin.concat(url) },
+        lastmod: { '_text': date.toISOString() },
+        changefreq: { '_text': 'never' },
+    }
+    let replaced = false;
+    const updatedUrls = sitemapMonth.urlset.urls.map((entry) => {
+        if (!entry.loc._text.startsWith(`${urlOrigin}${urlBase}/${payload.id}-`)) {
+            return entry;
+        }
+        if (entry.loc._text !== urlOrigin.concat(url)) {
+            return null;
+        }
+        replaced = true;
+        return {
+            ...entry,
+            ...sitemapEntry,
+        };
+    }).filter(e => !!e);
+    if (!replaced) {
+        updatedUrls.push(sitemapEntry);
+    }
+
+    sitemapMonth.urlset.urls = updatedUrls;
+    const sitemapMonthXML = js2xml(sitemapMonth, { compact: true });
+    await s3.putObject({
+        Key: `${urlBase}/sitemap.xml`,
+        Body: sitemapMonthXML,
+        ContentType: 'application/xml',
+        ACL: 'public-read',
+    }).promise();
 }
