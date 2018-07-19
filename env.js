d07bc609aecdbdab5ac2fca33c5d67493ff59cb7
Jannes Höke <jhoeke@uni-bremen.de>
2018-07-19 19:24:56 +0200

    :art: Refactor env loading

--- env.js
+++ env.js
@@ -1,7 +1,22 @@
 const fetchConfig = require('@wdr-data/s3-env-config').configure;
 
-module.exports.fetch = () => fetchConfig();
-module.exports.getStage = () => {
-  const env = fetchConfig();
+const getStage = async (envParam = null) => {
+  const env = envParam || await fetchConfig();
   return process.env.SLS_STAGE || env.DEPLOY_ALIAS || 'dev';
+};
+
+const getBucketName = async () => `informant-amp-web-${await getStage()}`;
+
+const fetch = async () => {
+  const env = await fetchConfig();
+  return {
+    ...env,
+    BUCKET_NAME: await getBucketName(),
+  };
+};
+
+module.exports = {
+  fetch,
+  getStage,
+  getBucketName,
 };
